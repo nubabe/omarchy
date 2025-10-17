2f075c1f21ac90aeb435cb06fdd0d6447ac79d70
nubabe <nubabe.me@gmail.com>
2025-10-17 11:50:26 +0200

    Removed sudo permissions for ufw-docker

--- install/preflight/first-run-mode.sh
+++ install/preflight/first-run-mode.sh
@@ -6,8 +6,11 @@ touch ~/.local/state/omarchy/first-run.mode
 sudo tee /etc/sudoers.d/first-run >/dev/null <<EOF
 Cmnd_Alias FIRST_RUN_CLEANUP = /bin/rm -f /etc/sudoers.d/first-run
 $USER ALL=(ALL) NOPASSWD: /usr/bin/ufw
-$USER ALL=(ALL) NOPASSWD: /usr/bin/ufw-docker
+
 $USER ALL=(ALL) NOPASSWD: /usr/bin/gtk-update-icon-cache
 $USER ALL=(ALL) NOPASSWD: FIRST_RUN_CLEANUP
 EOF
 sudo chmod 440 /etc/sudoers.d/first-run
+
+# empty line:
+# $USER ALL=(ALL) NOPASSWD: /usr/bin/ufw-docker

ad7cd9bda480266f2733d906ce10c35e997896aa
nubabe <nubabe.me@gmail.com>
2025-10-23 16:08:56 +0200

    Manually added webapps/tuis are added to install

--- install/packaging/webapps.sh
+++ install/packaging/webapps.sh
@@ -4,3 +4,5 @@ omarchy-webapp-install "YouTube" https://youtube.com/ YouTube.png
 omarchy-webapp-install "GitHub" https://github.com/ GitHub.png
 omarchy-webapp-install "Discord" https://discord.com/channels/@me Discord.png
 omarchy-webapp-install "Zoom" https://app.zoom.us/wc/home Zoom.png "omarchy-webapp-handler-zoom %u" "x-scheme-handler/zoommtg;x-scheme-handler/zoomus"
+
+# webapps added by install script
